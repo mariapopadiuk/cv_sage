b4cb947f57a3c7affb8b90eda8423b6093c10efa
mariapopadiuk <mpopadiuk13@gmail.com>
2022-12-08 17:04:44 +0000

    section left done

--- resources/views/partials/content.blade.php
+++ resources/views/partials/content.blade.php
@@ -1,51 +1,50 @@
 <div class="wrapper p-5 flex">
 
-  <section class="section-left w-1/3 bg-[#161820] mr-5">
+  <section class="section-left w-1/3 bg-[#161820] mr-5 p-10 rounded-lg shadow-lg shadow-green-500/40">
     <div class="contect-section-left text-center">
       <div class="name-content text-white">
-        <img src="" alt="">
-        <h1 class="">Maria Popaiuk</h1>
-        <h3>Web Developer</h3>
+        <img class="rounded-full w-1/2 mx-auto mb-10" src="../../images/IMG4628.jpg" alt="my-foto" />
+        <h1 class="font-bold text-4xl">Maria Popaiuk</h1>
+        <h3 class="font-extralight text-2xl mb-10">Web Developer</h3>
       </div>
-      <div class="social-content">
-        <ul class="text-white">
-          <li>+ 351 934 113 417</li>
-          <li>mpopadiuk13@gmail.com</li>
-          <li>Ukranian</li>
-          <li>03/03/1995</li>
-          <li>social icons</li>
+      <div class="social-content mb-20">
+        <ul class="text-white text-xl">
+          <li><a href="#"><i class="fa-solid fa-phone text-[#029D36]"></i> + 351 934 113 417</a></li>
+          <li><a href="#"><i class="fa-regular fa-envelope text-[#029D36]"></i> mpopadiuk13@gmail.com</a></li>
+          <li><i class="fa-regular fa-user text-[#029D36]"></i> Ukranian</li>
+          <li class="mb-10"><i class="fa-regular fa-calendar text-[#029D36]"></i> 03/03/1995</li>
+          <li><a href="#"><i class="fa-brands fa-instagram text-[#029D36]"></i></a> <a href="#"><i class="fa-brands fa-github text-[#029D36]"></i></a> <a href="#"><i class="fa-brands fa-linkedin text-[#029D36]"></i></a> <a href="#"><i class="fa-brands fa-500px text-[#029D36]"></i></a></li>
         </ul>
       </div>
       <div class="btn-pdf">
-        <button class="bg-[#029D36] p-3 text-white rounded-full" type="button">Downloud PDF</button>
+        <button class="bg-[#029D36] px-10 py-3 text-white rounded-full text-xl font-bold pointer" type="button">Downloud PDF</button>
       </div>
     </div>
   </section>
 
   <section class="section-right w-2/3">
-
-    <div class="block-about bg-[#161820]">
+    <div class="block-about bg-[#161820] shadow-lg shadow-green-500/40">
       <div class="text-white">
         <h1 class="">About</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim veniam debitis esse voluptates eaque, facere eligendi nemo quasi, minus excepturi vitae voluptatem quam? Quibusdam quaerat beatae, laboriosam explicabo harum ad!</p>
       </div>
     </div>
 
-    <div class="block-about bg-[#161820] mt-5">
+    <div class="block-about bg-[#161820] mt-5 shadow-lg shadow-green-500/40">
       <div class="text-white">
         <h1 class="">EDUCATION AND TRANING</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim veniam debitis esse voluptates eaque, facere eligendi nemo quasi, minus excepturi vitae voluptatem quam? Quibusdam quaerat beatae, laboriosam explicabo harum ad!</p>
       </div>
     </div>
 
-    <div class="block-about bg-[#161820] mt-5">
+    <div class="block-about bg-[#161820] mt-5 shadow-lg shadow-green-500/40">
       <div class="text-white">
         <h1 class="">EDUCATION AND TRANING</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim veniam debitis esse voluptates eaque, facere eligendi nemo quasi, minus excepturi vitae voluptatem quam? Quibusdam quaerat beatae, laboriosam explicabo harum ad!</p>
       </div>
     </div>
 
-    <div class="block-about bg-[#161820] mt-5">
+    <div class="block-about bg-[#161820] mt-5 shadow-lg shadow-green-500/40">
       <div class="text-white">
         <h1 class="">EDUCATION AND TRANING</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim veniam debitis esse voluptates eaque, facere eligendi nemo quasi, minus excepturi vitae voluptatem quam? Quibusdam quaerat beatae, laboriosam explicabo harum ad!
